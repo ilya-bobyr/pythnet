6fb77d94c1bbb30ffb85e8d9032ccda09f4ecab0
Illia Bobyr <illia.bobyr@gmail.com>
2025-07-22 05:42:20 -0700

    1.14 compat: Restore `consume_blockstore_duplicate_proofs`
    Reverts
    
    ```gitcommit
    commit 5199bd0afaef0c2fdd1813372930911c0060104f
    Author: Ashwin Sekar <ashwin@anza.xyz>
    Date:   Tue May 7 15:03:27 2024 -0700
    
        ff cleanup: consume_blockstore_duplicate_proofs (#1235)
    ```
    
    Necessary to make 2.0 compatible with 1.14, by restoring the conditional
    logic controlled by the `consume_blockstore_duplicate_proofs` feature
    gate.

--- core/src/replay_stage.rs
+++ core/src/replay_stage.rs
@@ -1417,7 +1417,9 @@ impl ReplayStage {
                 .unwrap();
             let duplicate_slot_hashes = duplicate_slots.filter_map(|slot| {
                 let bank = bank_forks.get(slot)?;
-                Some((slot, bank.hash()))
+                bank.feature_set
+                    .is_active(&feature_set::consume_blockstore_duplicate_proofs::id())
+                    .then_some((slot, bank.hash()))
             });
             (
                 root_bank,
@@ -2315,7 +2317,11 @@ impl ReplayStage {
         );
 
         // If we previously marked this slot as duplicate in blockstore, let the state machine know
-        if !duplicate_slots_tracker.contains(&slot) && blockstore.get_duplicate_slot(slot).is_some()
+        if bank
+            .feature_set
+            .is_active(&feature_set::consume_blockstore_duplicate_proofs::id())
+            && !duplicate_slots_tracker.contains(&slot)
+            && blockstore.get_duplicate_slot(slot).is_some()
         {
             let duplicate_state = DuplicateState::new_from_state(
                 slot,
@@ -3203,7 +3209,10 @@ impl ReplayStage {
                     SlotStateUpdate::BankFrozen(bank_frozen_state),
                 );
                 // If we previously marked this slot as duplicate in blockstore, let the state machine know
-                if !duplicate_slots_tracker.contains(&bank.slot())
+                if bank
+                    .feature_set
+                    .is_active(&feature_set::consume_blockstore_duplicate_proofs::id())
+                    && !duplicate_slots_tracker.contains(&bank.slot())
                     && blockstore.get_duplicate_slot(bank.slot()).is_some()
                 {
                     let duplicate_state = DuplicateState::new_from_state(
